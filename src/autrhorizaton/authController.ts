96b8104f5abc8f6e4598410c05af8e56c54c1e3f
eniorsantos <eniorsantos@hotmail.com>
2024-07-31 00:04:23 -0300

    maquina 2 atualiza

--- src/autrhorizaton/authController.ts
+++ src/autrhorizaton/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import userModel from '../model/userModel';
 import bcryptjs from 'bcryptjs';
+import jwt from "jsonwebtoken"
 
 type Usuario = {
   nome: string;
@@ -19,23 +20,36 @@ export const register = async (request: Request, response: Response) => {
   return response.status(201).json({ message: 'ok' });
 };
 
-type Acesso = {
+type BaseAuth = {
   email: string;
   passwd: string;
 };
 export const authenticate = async (request: Request, response: Response) => {
-  const acesso: Acesso = request.body;
-  const user = await userModel.findOne({ email: acesso.email });
+  try {
+    const body: BaseAuth = request.body;
+    const usuario = await userModel.findOne({ email: body.email });
+    
 
-  return response.status(201).json({ user });
+    if (!usuario) {
+      return response.status(404).json({ message: "Usuario não cadastrado!" });
+    }
 
-  //console.log(acesso)
+    const testpasswd = bcryptjs.compareSync(body.passwd, usuario.passwd);
+    
+    if (!testpasswd) {
+      return response
+        .status(400)
+        .json({ message: "Senha e/ou e-mail incorretos!" });
+    }
 
-  //
+    const token = jwt.sign({id:usuario._id}, process.env.SECRET_KEY || " ")
 
-  // const validador = bcryptjs.compareSync(acesso.passwd, user.passwd)
-
-  //
-
-  return response.status(200).json({ message: 'ok' });
+    return response
+      .status(201)
+      .json({ email: usuario.email, nome: usuario.nome, token: token });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ message: "Erro ao carregar dados " + error });
+  }
 };
